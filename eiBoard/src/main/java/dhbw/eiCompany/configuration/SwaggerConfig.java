eeff0ef9d0e44c52766d093178cb02943a44f04e
Matteo Staar <matteo.staar@cjt.de>
2023-04-13 14:45:37 +0200

    init of swagger

---
+++ eiBoard/src/main/java/dhbw/eiCompany/configuration/SwaggerConfig.java
@@ -0,0 +1,36 @@
+package dhbw.eiCompany.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.OAuthFlow;
+import io.swagger.v3.oas.models.security.OAuthFlows;
+import io.swagger.v3.oas.models.security.Scopes;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+
+@Configuration
+public class SwaggerConfig {
+
+	//ADD KEYCLOAK TOKEN
+	private final static String TOKENURL = "http://localhost:8081/realms/SpringBootKeycloak/protocol/openid-connect/token";
+
+	@Bean
+	public OpenAPI springShopOpenAPI() {
+		OAuthFlows flows = new OAuthFlows();
+		OAuthFlow flow = new OAuthFlow();
+
+		flow.setTokenUrl(TOKENURL);
+
+		Scopes scopes = new Scopes();
+		flow.setScopes(scopes);
+		flows = flows.password(flow);
+
+		return new OpenAPI()
+				.components(new Components().addSecuritySchemes("keycloak",
+						new SecurityScheme().type(SecurityScheme.Type.OAUTH2).flows(flows)))
+				.info(new Info().title("EiCompany").description("Swagger UI for the EiCompany Backend").version("0.0.1"));
+	}
+}
--- eiBoard/src/main/java/dhbw/eiCompany/controller/UserController.java
+++ eiBoard/src/main/java/dhbw/eiCompany/controller/UserController.java
@@ -1,14 +1,24 @@
 package dhbw.eiCompany.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import dhbw.eiCompany.database.Person;
 import dhbw.eiCompany.interfaces.PersonService;
-
-import java.util.List;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 
 @RestController
 public class UserController {
@@ -21,6 +31,12 @@ public class UserController {
         return new ResponseEntity<>(personService.findByName(name), HttpStatus.OK);
     }
 
+
+	@Operation(summary = "List all users", description = "List all existing users")
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "200", description = "Returns users successfully", content = @Content(schema = @Schema(implementation = Person.class))),
+			@ApiResponse(responseCode = "401", description = "Unauthorized", content = @Content()),
+			@ApiResponse(responseCode = "403", description = "Unauthorized", content = @Content()), })
     @GetMapping(path = "/user")
     public ResponseEntity<List<Person>> getAllUser(){
         return new ResponseEntity<>(personService.getAllUser(), HttpStatus.OK);
