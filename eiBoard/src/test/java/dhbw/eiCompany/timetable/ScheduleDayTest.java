d0a89ef8a2e37d11a20e331d705b4e0e90e7f669
Julian Stadler <jstadler@testo.de>
2023-05-16 10:19:41 +0200

    implemented Unit Tests for Class ScheduleDay

--- eiBoard/src/test/java/dhbw/eiCompany/timetable/ScheduleDayTest.java
+++ eiBoard/src/test/java/dhbw/eiCompany/timetable/ScheduleDayTest.java
@@ -3,56 +3,64 @@ package dhbw.eiCompany.timetable;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.util.Assert;
 
+import java.lang.reflect.Array;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class ScheduleDayTest {
     ScheduleDay testDay = new ScheduleDay();
-    Lecture lecture1;
-    Lecture lecture2;
-    private List<Lecture> testLectureList = new ArrayList<Lecture>();
+    Lecture lecture1 = new Lecture(
+            "testLecturer",
+            "testRoom",
+            "testLecture",
+            LocalDateTime.now(),
+            LocalDateTime.now().plusDays(1),
+            LocalDate.now()
+    );;
+    Lecture lecture2 = new Lecture(
+            "Mr.Eisenbiegler",
+            "F419",
+            "Compilerbau",
+            LocalDateTime.now(),
+            LocalDateTime.now().plusDays(1),
+            LocalDate.now()
+    );
 
     @BeforeEach
     void setUp() {
-
-        lecture1 = new Lecture(
-                "testLecturer",
-                "testRoom",
-                "testLecture",
-                LocalDateTime.now(),
-                LocalDateTime.now().plusDays(1),
-                LocalDate.now()
-        );
-
-        lecture2 = new Lecture(
-                "Mr.Eisenbiegler",
-                "F419",
-                "Compilerbau",
-                LocalDateTime.now(),
-                LocalDateTime.now().plusDays(1),
-                LocalDate.now()
-        );
-
+        testDay.addLecture(lecture1);
+        testDay.addLecture(lecture2);
     }
 
     @Test
     void addLecture() {
-        testDay.addLecture(lecture1);
-        testDay.addLecture(lecture2);
-
-        assertEquals(testLectureList.size(), 0);
+        assertEquals(2, testDay.getLectureList().size());
     }
 
     @Test
     void getLectureList() {
+        List<Lecture> testLectureList1 = new ArrayList<Lecture>();
+        testLectureList1.add(lecture1);
+        testLectureList1.add(lecture2);
+
+        List<Lecture> testLectureList2 = new ArrayList<Lecture>();
+
+        assertThat(testDay.getLectureList())
+                .isEqualTo(testLectureList1)
+                .isNotEqualTo(testLectureList2);
     }
 
     @Test
     void clearAll() {
+        testDay.clearAll();
+
+        assertEquals(0, testDay.getLectureList().size());
     }
 }
